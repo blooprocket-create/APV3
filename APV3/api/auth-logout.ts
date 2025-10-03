acfd73b85cb4c52bfeea2de56280bbdab2e71298
blooprocket-create <blooprocket@gmail.com>
2025-10-03 17:45:29 -0400

    Delete APV3 directory

--- APV3/api/auth-logout.ts
+++
@@ -1,9 +0,0 @@
-import type { VercelRequest, VercelResponse } from "@vercel/node";
-import { asyncHandler, requireMethod } from "../lib/utils";
-import { clearAuthCookie } from "../lib/auth";
-
-export default asyncHandler(async (req: VercelRequest, res: VercelResponse) => {
-  if (!requireMethod(req, res, ["POST"])) return;
-  clearAuthCookie(res);
-  res.status(200).json({ success: true });
-});
--- APV3/lib/env.ts
+++
@@ -1,5 +0,0 @@
-import dotenv from "dotenv";
-
-if (!process.env.VERCEL) {
-  dotenv.config();
-}
